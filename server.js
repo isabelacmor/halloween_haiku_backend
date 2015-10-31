d8fb20a4107c20eed405eb9e5e31dec1ef2af911
Tyler Leonhardt <tylerleonhardt@Tylers-MacBook-Pro-2.local>
2015-10-31 01:10:01 -0400

    redis store

--- server.js
+++ server.js
@@ -2,28 +2,45 @@ var io = require('socket.io')(80);
 var cfg = require('./config.json');
 var tw = require('node-tweet-stream')(cfg);
 var syllable = require('syllable');
+var redis = require("redis").createClient();
+
+redis.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+var tweetWith5 = {};
+var tweetWith7 = {};
+
 tw.track('#halloween');
 tw.on('tweet', function(tweet){
   io.emit('tweet', tweet.text);
   var trimmedTweet = trimTweet(" " + tweet.text);
   var syl = getSyllables(trimmedTweet);
-  if((syl == 5 || syl == 7) && !trimmedTweet.match(/\s+RT\s+|[0-9]/g))
-    console.log(trimmedTweet);
-    // console.log(tweet);
+  if(!trimmedTweet.match(/\s+RT\s+|[0-9]/g)){
+    if(syl == 5){
+      tweetWith5.tweet = trimmedTweet.trim();
+      tweetWith5.url = "https://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str;
+      // console.log(tweetWith5);
+    }else if (syl == 7) {
+      tweetWith7.tweet = trimmedTweet.trim();
+      tweetWith7.url = "https://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str;
+      // console.log(tweetWith7);
+    }
+  }
 });
 
 function trimTweet(text){
 
-  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-  var regex = new RegExp(expression);
-  text = text.replace(regex, "");
+  //remove links
+  var noLinks = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  text = text.replace(noLinks, "");
+
+  //remove non alphanumeric chars
   text = text.replace(/[\W_]/g, ' ');
+
+  //remove extra whitespace
   text = text.replace(/\s\s+/g, ' ');
-  // text = text.replace(new RegExp("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"), " ");
-  text = text.replace(new RegExp("[^\x00-\x7F]+"), " ");
-  return text = text.replace(new RegExp("[^\u0000-\u007F]+"), " ");
-  //console.log("wow:" + text);
-  //return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",text).split())
+  return text;
 }
 
 function getSyllables(str){
@@ -37,6 +54,38 @@ function getSyllables(str){
 
   return count;
 }
-// var test = trimTweet("My halloween game face");
-// console.log(test);
-// console.log(getSyllables(trimTweet("My halloween game face")));
+
+function getDate(){
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  if(dd<10) {
+      dd='0'+dd
+  }
+
+  if(mm<10) {
+      mm='0'+mm
+  }
+
+  today = mm+'/'+dd+'/'+yyyy;
+  return today;
+}
+
+function start(){
+  setTimeout(function(){
+    var data = {
+      tweetWith5:tweetWith5,
+      tweetWith7:tweetWith7,
+      today:getDate()
+    }
+    console.log(data);
+
+    redis.rpush("haikus", JSON.stringify(data), function(){
+      console.log(data);
+    });
+    start();
+  }, 30000);
+}
+start();
